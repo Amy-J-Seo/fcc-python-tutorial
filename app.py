d3a457bd66a475621e7d7e600b3f83fe1ba61cdb
Amy-j-seo <amywonderland77@gmail.com>
2023-08-30 11:01:31 +0900

    trycatch

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-# name ='test22'
-
-# print('Hello world '+ name)
-# print('welcome')
-
-# name = input('Enter something :');
-# print(name)
-
-sentence = input('enter a sentence: ')
-print("Your sentence is ", sentence)
-
-word1 = input('Enter the word where to replace ')
-word2 = input('Enter a word to replace ')
-print(sentence.replace(word1, word2))
+try:
+    x = int(input('Enter a number: '))
+    print(x)
+except ValueError:
+    print("Something went wrong", ValueError)
+except NameError:
+    print('name?')
+else:
+    print('nothing went wrong')
+finally:
+    print('finished')
