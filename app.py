46dba70a448270d290ba81ee8059223ff448c6c3
Amy-j-seo <amywonderland77@gmail.com>
2023-08-30 10:51:07 +0900

    input&replace

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-print('Hello world')
-print('welcome')
+# name ='test22'
 
+# print('Hello world '+ name)
+# print('welcome')
+
+# name = input('Enter something :');
+# print(name)
+
+sentence = input('enter a sentence: ')
+print("Your sentence is ", sentence)
+
+word1 = input('Enter the word where to replace ')
+word2 = input('Enter a word to replace ')
+print(sentence.replace(word1, word2))
